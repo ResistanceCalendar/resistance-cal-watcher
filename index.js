b370895abca875976cc01130bbf35094f52876d3
aaghevli <arash.aghevli@gmail.com>
2017-08-13 14:23:57 -0700

    remove unused method

--- index.js
+++ index.js
@@ -38,11 +38,6 @@ function getFacebookEvents (accountName) {
   return getPage(`https://graph.facebook.com/v2.8/${accountName}/events?access_token=${process.env.FB_TOKEN}&limit=100&fields=id,name,description,start_time,attending_count,place,interested_count`);
 }
 
-function checkForEvents (events) {
-  // TODO: Check resistence calendar API for the event and filter out
-  return events;
-}
-
 function postNewEvent (event, source) {
   return request({
     uri: process.env.SLACK_ENDPOINT,
